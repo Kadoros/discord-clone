f4c23f63ffd79a0450a20de38e4e1fca2fab9d0d
kado <kadoros1130@gmail.com>
2024-12-14 01:11:22 +0100

    fix cors

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -23,6 +23,10 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   const io = new ServerIO(httpServer, {
     path: path,
     addTrailingSlash: false,
+    cors: {
+      origin: "https://wisto.vercel.app",
+      methods: ["GET", "POST"],
+    },
   });
 
   // server-side connection handler
