2678f382c89b6e21881617ce4c7cf573ba6d8903
kado <kadoros1130@gmail.com>
2024-12-14 01:15:09 +0100

    io transports setup

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -23,6 +23,8 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   const io = new ServerIO(httpServer, {
     path: path,
     addTrailingSlash: false,
+    transports: ["websocket", "polling"],
+    // reconnectionAttempts: 5,
     cors: {
       origin: "https://wisto.vercel.app",
       methods: ["GET", "POST"],
